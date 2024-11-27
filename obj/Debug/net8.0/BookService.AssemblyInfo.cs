33c8dfdd4f8a4b635f5fdcca436d260932639cb8
ThanhNgan97 <nguyenthanhngan09072002@gmail.com>
2024-11-28 00:21:27 +0700

    API

--- obj/Debug/net8.0/BookService.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9302467af44b483bad314e053770546699469bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4189bb57cad14d656be70c1127f31bb791ff5550")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
